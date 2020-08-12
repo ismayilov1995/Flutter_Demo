ea803333b257465df640a479bb4ff3f06161cf2b
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-12 22:40:32 +0400

    Container margin padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.orange, accentColor: Colors.yellow),
-    debugShowCheckedModeBanner: false,
-    home: new Scaffold(
+      theme:
+          ThemeData(primarySwatch: Colors.orange, accentColor: Colors.yellow),
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
         appBar: AppBar(
           title: Text(
             'Salam Ahmet',
@@ -27,16 +29,18 @@ void main() {
             height: 50.0,
           ),
         ),
-        body: Center(
-          child: Container(
-            color: Colors.teal[50],
-            constraints: BoxConstraints(
-                maxHeight: 300, maxWidth: 300, minHeight: 200, minWidth: 200),
-            child: Text(
-              "Per petux",
-              style: TextStyle(fontSize: 36),
-            ),
+        body: Container(
+          color: Colors.teal[50],
+          margin: EdgeInsets.all(20.0),
+          padding: EdgeInsets.only(top: 20, left: 30),
+          constraints: BoxConstraints.expand(
+            width: 400,
+            height: 400,
           ),
-        )),
-  ));
+          child: Text(
+            "Per petux",
+            style: TextStyle(fontSize: 36),
+          ),
+        ),
+      )));
 }
