1fba79549116e6f7af8ba1e304065ac9a70fa440
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-13 22:33:12 +0400

    GridView.builder

--- lib/ui/stateless_gridview.dart
+++ lib/ui/stateless_gridview.dart
@@ -3,78 +3,26 @@ import 'package:flutter/material.dart';
 class AppGridView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView.extent(
-      primary: true,
-      padding: EdgeInsets.all(10),
-      crossAxisSpacing: 20,
-      mainAxisSpacing: 20,
-      maxCrossAxisExtent: 300,
-      scrollDirection: Axis.vertical,
-      children: [
-        Container(
-          color: Colors.teal[300],
+    return GridView.builder(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+      itemCount: 100,
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          color: Colors.teal[100 * ((index + 1) % 8)],
           alignment: Alignment.center,
           child: Text(
-            "Hello flutter",
+            "Hello flutter $index",
             textAlign: TextAlign.center,
           ),
-        ),
-        Container(
-          color: Colors.teal[300],
-          alignment: Alignment.center,
-          child: Text(
-            "Hello flutter",
-            textAlign: TextAlign.center,
-          ),
-        ),
-        Container(
-          color: Colors.teal[300],
-          alignment: Alignment.center,
-          child: Text(
-            "Hello flutter",
-            textAlign: TextAlign.center,
-          ),
-        ),
-        Container(
-          color: Colors.teal[300],
-          alignment: Alignment.center,
-          child: Text(
-            "Hello flutter",
-            textAlign: TextAlign.center,
-          ),
-        ),
-        Container(
-          color: Colors.teal[300],
-          alignment: Alignment.center,
-          child: Text(
-            "Hello flutter",
-            textAlign: TextAlign.center,
-          ),
-        ),
-        Container(
-          color: Colors.teal[300],
-          alignment: Alignment.center,
-          child: Text(
-            "Hello flutter",
-            textAlign: TextAlign.center,
-          ),
-        ),
-        Container(
-          color: Colors.teal[300],
-          alignment: Alignment.center,
-          child: Text(
-            "Hello flutter",
-            textAlign: TextAlign.center,
-          ),
-        ),
-        Container(
-          color: Colors.teal[300],
-          alignment: Alignment.center,
-          child: Text(
-            "Hello flutter",
-            textAlign: TextAlign.center,
-          ),
-        ),
+        );
+      },
+    );
+  }
+}
+
+/*
+children: [
         Container(
           color: Colors.teal[300],
           alignment: Alignment.center,
@@ -84,6 +32,4 @@ class AppGridView extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
+ */
