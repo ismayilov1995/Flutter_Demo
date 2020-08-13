08c61a2f2b9be4d24f4e558eaf906f8bd21e52c2
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-14 01:11:28 +0400

    SliverList

--- lib/ui/collapsable_toolbar.dart
+++ lib/ui/collapsable_toolbar.dart
@@ -6,14 +6,89 @@ class AppCollapsableToolbar extends StatelessWidget {
     return CustomScrollView(
       slivers: <Widget>[
         SliverAppBar(
-          title: Text('Sliver AppBar'),
           backgroundColor: Colors.orangeAccent,
-          expandedHeight: 200,
-          floating: true,
+          expandedHeight: 300,
+          floating: false,
+          // snap: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text('Sliver AppBar'),
+            centerTitle: true,
+            background: Image.asset("assets/images/icon.jpg"),
+          ),
+          pinned: true,
         ),
-//        SliverList(),
-//        SliverGrid(),
+        SliverList(
+          delegate: SliverChildListDelegate(staticListItems()),
+        )
       ],
     );
+  }
+
+  List<Widget> staticListItems() {
+    return [
+      Container(
+        height: 100,
+        color: Colors.red[100],
+        child: FlutterLogo(
+          colors: Colors.orange,
+          style: FlutterLogoStyle.horizontal,
+          textColor: Colors.indigo,
+        ),
+      ),
+      Container(
+        height: 100,
+        color: Colors.red[200],
+        child: FlutterLogo(
+          colors: Colors.orange,
+          style: FlutterLogoStyle.horizontal,
+          textColor: Colors.indigo,
+        ),
+      ),
+      Container(
+        height: 100,
+        color: Colors.red[300],
+        child: FlutterLogo(
+          colors: Colors.orange,
+          style: FlutterLogoStyle.horizontal,
+          textColor: Colors.indigo,
+        ),
+      ),
+      Container(
+        height: 100,
+        color: Colors.red[400],
+        child: FlutterLogo(
+          colors: Colors.orange,
+          style: FlutterLogoStyle.horizontal,
+          textColor: Colors.indigo,
+        ),
+      ),
+      Container(
+        height: 100,
+        color: Colors.red[500],
+        child: FlutterLogo(
+          colors: Colors.grey,
+          style: FlutterLogoStyle.horizontal,
+          textColor: Colors.grey[50],
+        ),
+      ),
+      Container(
+        height: 100,
+        color: Colors.red[600],
+        child: FlutterLogo(
+          colors: Colors.yellow,
+          style: FlutterLogoStyle.horizontal,
+          textColor: Colors.yellow,
+        ),
+      ),
+      Container(
+        height: 100,
+        color: Colors.blue[600],
+        child: FlutterLogo(
+          colors: Colors.yellow,
+          style: FlutterLogoStyle.horizontal,
+          textColor: Colors.white,
+        ),
+      ),
+    ];
   }
 }
