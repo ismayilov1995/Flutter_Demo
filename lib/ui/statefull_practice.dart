34a605a30a683fafd657a3ff6493987447c46d51
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-13 16:38:00 +0400

    Stateful increase decrease counter

--- lib/ui/statefull_practice.dart
+++ lib/ui/statefull_practice.dart
@@ -10,19 +10,31 @@ class StatefullPractice extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   final String title;
+  int counter = 0;
 
   MyHomePage({this.title}) {}
 
   @override
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(widget.title),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            increaseCounter();
+          });
+        },
         child: Icon(Icons.ac_unit),
       ),
       body: Center(
@@ -31,13 +43,29 @@ class MyHomePage extends StatelessWidget {
           children: [
             Text("Welcome to flutter you have pressed"),
             Text(
-              "5",
+              "${widget.counter}",
               style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
             ),
             Text("times"),
+            RaisedButton(
+              onPressed: () {
+                setState(() {
+                  decreaseCounter();
+                });
+              },
+              child: Text('Decrease'),
+            )
           ],
         ),
       ),
     );
+  }
+
+  void increaseCounter() {
+    widget.counter++;
+  }
+
+  void decreaseCounter() {
+    widget.counter--;
   }
 }
