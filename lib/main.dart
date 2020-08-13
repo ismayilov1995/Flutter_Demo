1dc7db6dd20ea88256250f32352f68eee8025231
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-13 14:24:20 +0400

    Image and row

--- lib/main.dart
+++ lib/main.dart
@@ -20,99 +20,72 @@ void main() {
       ),
       body: Column(
         mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           Text(
             'Image and Button types',
             style: TextStyle(fontSize: 18),
           ),
-          Expanded(
-            child: Container(
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    alignment: Alignment.center,
+          IntrinsicHeight(
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(4),
                     color: Colors.red[300],
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
                         Image.asset("assets/images/icon.jpg"),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    alignment: Alignment.center,
-                    color: Colors.red[300],
-                    child: Column(
-                      children: [
-                        Image.network(
-                            "https://i.picsum.photos/id/942/200/200.jpg?hmac=Gh7W-H3ZGmweB9STLwQvq-IHkxrVyawHVTKYxy-u9mA"),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        CircleAvatar(
-                          backgroundImage: NetworkImage("https://i.picsum.photos/id/980/200/200.jpg?hmac=6XJlc3jZzO4_ikuKGQFXIuERlW0eZx82q3xiC-b3Tso"),
-                          maxRadius: 50,
-                          minRadius: 25,
+                        Text(
+                          'Salam',
+                          style: TextStyle(fontSize: 16),
                         )
                       ],
                     ),
                   ),
-                ],
-              ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(4),
+                    color: Colors.red[50],
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.network(
+                            "https://i.picsum.photos/id/942/200/200.jpg?hmac=Gh7W-H3ZGmweB9STLwQvq-IHkxrVyawHVTKYxy-u9mA"),
+                        Text(
+                          'Salam',
+                          style: TextStyle(fontSize: 16),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(4),
+                    color: Colors.orange[50],
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              "https://i.picsum.photos/id/980/200/200.jpg?hmac=6XJlc3jZzO4_ikuKGQFXIuERlW0eZx82q3xiC-b3Tso"),
+                          radius: 46,
+                        ),
+                        Text(
+                          'Salam',
+                          style: TextStyle(fontSize: 16),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-          Expanded(
-            child: Container(
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 100,
-                    height: 100,
-                    alignment: Alignment.center,
-                    color: Colors.red[300],
-                    child: Text(
-                      'D',
-                      style: TextStyle(fontSize: 26),
-                    ),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    alignment: Alignment.center,
-                    color: Colors.red[300],
-                    child: Text(
-                      'D',
-                      style: TextStyle(fontSize: 26),
-                    ),
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    alignment: Alignment.center,
-                    color: Colors.red[300],
-                    child: Text(
-                      'D',
-                      style: TextStyle(fontSize: 26),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          )
         ],
       ),
     ),
