50e40d9f439cc01c9aa6a2046ab2aa7a714b8fee
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-13 19:23:48 +0400

    Separated ListView awesome feature

--- lib/ui/aficion_samples.dart
+++ lib/ui/aficion_samples.dart
@@ -1,9 +1,90 @@
 import 'package:flutter/material.dart';
 
 class AficionListSamples extends StatelessWidget {
+  List<Student> allStudents = [];
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 1000, itemBuilder: (context, index) => Text("Item $index"));
+    fetchStudents();
+    return ListView.separated(
+        separatorBuilder: (context, index) {
+          if (index % 4 == 0 && index != 0) {
+            return OptionButton();
+          } else {
+            return Divider();
+          }
+        },
+        itemCount: allStudents.length,
+        itemBuilder: (context, index) => Card(
+              margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              color:
+                  allStudents[index]._gender ? Colors.teal[50] : Colors.red[50],
+              elevation: 8,
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.face),
+                ),
+                title: Text(allStudents[index]._name),
+                subtitle: Text(allStudents[index]._email),
+                trailing: allStudents[index]._gender
+                    ? Icon(Icons.lock_open)
+                    : Icon(Icons.wifi),
+                onLongPress: () {
+                  debugPrint(allStudents[index]._name);
+                },
+              ),
+            ));
+  }
+
+  void fetchStudents() {
+    allStudents = List.generate(
+        300,
+        (index) => Student("Ismayil $index", "ismayil$index@mail.ru",
+            index % 2 == 0 ? true : false));
+  }
+}
+
+class Student {
+  String _name;
+  String _email;
+  bool _gender;
+
+  Student(this._name, this._email, this._gender);
+}
+
+class OptionButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        RaisedButton(
+          color: Colors.indigo,
+          child: Text(
+            'Guard',
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () {},
+        ),
+        RaisedButton(
+          color: Colors.red,
+          child: Row(
+            children: [
+              Icon(
+                Icons.remove_circle_outline,
+                color: Colors.white,
+              ),
+              Text(
+                'Remove',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
   }
 }
