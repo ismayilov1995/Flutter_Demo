3111527bacfa8c74f3f1dbcd5b36204e83053919
ismayilov1995 <ismayilov1995@gmail.com>
2020-08-13 18:20:46 +0400

    Generate 300 item array and fetch ListView

--- lib/ui/list_demos.dart
+++ lib/ui/list_demos.dart
@@ -1,63 +1,42 @@
 import 'package:flutter/material.dart';
 
 class ListDemos extends StatelessWidget {
+  // Dummy data
+  List<int> listNum = List.generate(300, (index) => index);
+  List<String> listTitle =
+      List.generate(300, (index) => 'Ismayil Ismayilov $index');
+  List<String> listSubtitle =
+      List.generate(300, (index) => 'I am Subtitle $index');
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          child: Card(
-            margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
-            color: Colors.grey[200],
-            elevation: 4,
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Icon(Icons.face),
+    return ListView(
+      children: listNum
+          .map(
+            (item) => Container(
+              child: Card(
+                margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
+                color: Colors.grey[200],
+                elevation: 4,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    child: Icon(Icons.face),
+                  ),
+                  title: Text(listTitle[item]),
+                  subtitle: Text(listSubtitle[item]),
+                  trailing: Icon(Icons.lock_open),
+                ),
               ),
-              title: Text('Ataturk'),
-              subtitle: Text('Mustafa Kemal Masha'),
-              trailing: Icon(Icons.lock_open),
             ),
-          ),
-        ),
-//        Divider(
-//          indent: 20,
-//          endIndent: 20,
-//          color: Colors.grey,
-//        ),
-        Container(
-          child: Card(
-            margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
-            color: Colors.grey[200],
-            elevation: 4,
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Icon(Icons.face),
-              ),
-              title: Text('Ataturk'),
-              subtitle: Text('Mustafa Kemal Masha'),
-              trailing: Icon(Icons.lock_open),
-            ),
-          ),
-        ),
-        Container(
-          child: Card(
-            margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
-            color: Colors.grey[200],
-            elevation: 4,
-            child: ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Icon(Icons.face),
-              ),
-              title: Text('Ataturk'),
-              subtitle: Text('Mustafa Kemal Masha'),
-              trailing: Icon(Icons.lock_open),
-            ),
-          ),
-        ),
+          )
+          .toList(),
+    );
+  }
+}
+
+/*
+children: <Widget>[
         Container(
           child: Card(
             margin: EdgeInsets.fromLTRB(20, 20, 20, 0),
@@ -75,6 +54,4 @@ class ListDemos extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
+ */
